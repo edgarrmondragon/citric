da8aec87ffbcb9b502fc4487f1591a6d0e1b74f1
Edgar Ramírez-Mondragón <edgar@spoton.com>
2019-11-22 21:45:16 -0600

    Use markdown readme

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open('README.md', encoding='utf-8') as readme_file:
     readme = readme_file.read()
 
 with open('HISTORY.rst') as history_file:
@@ -42,6 +42,7 @@ setup(
     install_requires=requirements,
     license="MIT license",
     long_description=readme + '\n\n' + history,
+    long_description_content_type='text/markdown',
     include_package_data=True,
     keywords='limette',
     name='limette',
