67dacecfe1bf541544aaf6677eb46e08aa711796
Edgar Ramírez-Mondragón <edgar@spoton.com>
2019-11-22 22:35:12 -0600

    Use markdown history file in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 with open('README.md', encoding='utf-8') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open('HISTORY.md') as history_file:
     history = history_file.read()
 
 requirements = [
