6c5b6c286f7e89f11e1f992a674281e6f63106b4
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2022-02-04 23:32:30 -0600

    chore: remove flake8 and mypy workarounds

--- src/citric/__init__.py
+++ src/citric/__init__.py
@@ -2,14 +2,9 @@
 from citric.client import Client  # noqa: F401
 
 try:
-    from importlib.metadata import PackageNotFoundError, version  # type: ignore
-except ImportError:  # pragma: no cover
-    from importlib_metadata import PackageNotFoundError, version  # type: ignore
+    from importlib.metadata import version  # type: ignore
+except ImportError:
+    from importlib_metadata import version  # type: ignore
 
-
-try:
-    __version__ = version(__name__)
-    """Package version"""
-except PackageNotFoundError:  # pragma: no cover
-    __version__ = "unknown"
-    """Package version"""
+__version__ = version(__name__)
+"""Package version"""
