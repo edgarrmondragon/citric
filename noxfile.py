2b621e2abf8b78959b8856bcb4fb2aad342c66a1
Edgar R. M <edgarrm358@gmail.com>
2022-12-19 21:03:53 -0600

    chore: Test on PyPy 3.8 and 3.9 (#651)

--- noxfile.py
+++ noxfile.py
@@ -22,7 +22,7 @@ except ImportError:
 
 package = "citric"
 python_versions = ["3.12", "3.11", "3.10", "3.9", "3.8", "3.7"]
-pypy_versions = ["pypy3.9"]
+pypy_versions = ["pypy3.7", "pypy3.8", "pypy3.9"]
 main_python_version = "3.10"
 locations = "src", "tests", "noxfile.py", "docs/conf.py"
 nox.options.sessions = (
