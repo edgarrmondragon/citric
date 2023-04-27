cfb5981e54bd591e615e71b91786946e40cc6792
Edgar R. M <edgarrm358@gmail.com>
2023-04-26 22:06:05 -0600

    docs: Link to source code (#829)

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,7 @@ project_copyright = f"2020, {author}"
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.extlinks",
+    "sphinx.ext.linkcode",
     "sphinx.ext.napoleon",
     "sphinx_autodoc_typehints",
     "autoapi.extension",
@@ -68,3 +69,21 @@ source_suffix = {
 napoleon_custom_sections = [
     ("Keys", "params_style"),
 ]
+
+
+def linkcode_resolve(domain: str, info: dict) -> str | None:
+    """Get URL to source code.
+
+    Args:
+        domain: Language domain the object is in.
+        info: A dictionary with domain-specific keys.
+
+    Returns:
+        A URL.
+    """
+    if domain != "py":
+        return None
+    if not info["module"]:
+        return None
+    filename = info["module"].replace(".", "/")
+    return f"https://github.com/edgarrmondragon/citric/tree/main/src/{filename}.py"
