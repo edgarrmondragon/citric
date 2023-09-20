0ca58d3a398f8764118027c6719a37b9a6e75dc4
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2023-09-19 22:19:51 -0600

    XFail on 5.x

--- tests/integration/test_rest_client.py
+++ tests/integration/test_rest_client.py
@@ -2,9 +2,15 @@
 
 from __future__ import annotations
 
+import typing as t
+
 import pytest
+import requests
 
 from citric._rest import RESTClient
+
+if t.TYPE_CHECKING:
+    import semver
 
 
 @pytest.fixture(scope="module")
@@ -23,9 +29,21 @@ def rest_client(
 
 @pytest.mark.integration_test
 def test_get_surveys(
+    request: pytest.FixtureRequest,
     rest_client: RESTClient,
     survey_id: int,
+    server_version: semver.Version,
 ) -> None:
     """Test getting surveys."""
+    request.node.add_marker(
+        pytest.mark.xfail(
+            server_version < (6, 0, 0),
+            reason=(
+                "Quota RPC methods are not supported in LimeSurvey "
+                f"{server_version} < 6.0.0"
+            ),
+            raises=requests.exceptions.HTTPError,
+        ),
+    )
     surveys = rest_client.get_surveys()
     assert surveys[0]["sid"] == survey_id
